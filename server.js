627f0af7c818582b268ad752275bff4111d67f61
Pham Nguyen Minh Khoi <pnmkhoi@tma.com.vn>
2018-08-03 16:21:04 +0700

    update file

--- server.js
+++ server.js
@@ -16,14 +16,14 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 
 if (mongoURL == null && process.env.DATABASE_SERVICE_NAME) {
   var mongoServiceName = process.env.DATABASE_SERVICE_NAME.toUpperCase(),
-      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'],
-      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'],
-      mongoDatabase = process.env[mongoServiceName + '_DATABASE'],
+      mongoHost = process.env[mongoServiceName + '_SERVICE_HOST'] || 'localhost',
+      mongoPort = process.env[mongoServiceName + '_SERVICE_PORT'] || 27017,
+      mongoDatabase = process.env[mongoServiceName + '_DATABASE'] || 'smartcamera',
       mongoPassword = process.env[mongoServiceName + '_PASSWORD']
       mongoUser = process.env[mongoServiceName + '_USER'];
 
   if (mongoHost && mongoPort && mongoDatabase) {
-    mongoURLLabel = mongoURL = 'mongodb://';
+    mongoURLLabel = mongoURL = 'mongodb://localhost:27017/smartcamera';
     if (mongoUser && mongoPassword) {
       mongoURL += mongoUser + ':' + mongoPassword + '@';
     }
@@ -101,6 +101,62 @@ app.use(function(err, req, res, next){
 
 initDb(function(err){
   console.log('Error connecting to Mongo. Message:\n'+err);
+});
+
+//requiring path and fs modules
+const path = require('path');
+const fs = require('fs');
+//joining path of directory
+const directoryPath = path.join(__dirname, 'public/image');
+//passsing directoryPath and callback function
+fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    }
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file);
+    });
+});
+
+//lets require/import the mongodb native drivers.
+var mongodb = require('mongodb');
+
+//We need to work with "MongoClient" interface in order to connect to a mongodb server.
+var MongoClient = mongodb.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+var url = 'mongodb://localhost:27017/smartcamera';
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function (err, db) {
+    if (err) {
+        console.log('Unable to connect to the mongoDB server. Error:', err);
+    } else {
+        //HURRAY!! We are connected. :)
+        console.log('Connection established to', url);
+
+        // Get the documents collection
+        var collection = db.collection('images');
+
+        //Create some users
+        var user1 = {name: 'modulus admin', age: 42, roles: ['admin', 'moderator', 'user']};
+        var user2 = {name: 'modulus user', age: 22, roles: ['user']};
+        var user3 = {name: 'modulus super admin', age: 92, roles: ['super-admin', 'admin', 'moderator', 'user']};
+
+        // Insert some users
+        collection.insert([user1, user2, user3], function (err, result) {
+            if (err) {
+                console.log(err);
+            } else {
+                console.log('Inserted %d documents into the "images" collection. The documents inserted with "_id" are:', result.length, result);
+            }
+            //Close connection
+            db.close();
+        });
+    }
 });
 
 app.listen(port, ip);
